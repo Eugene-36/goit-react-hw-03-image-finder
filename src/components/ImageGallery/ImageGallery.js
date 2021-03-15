bb3d511734d25b36651f49445c3b6d92c77fb832
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-03-15 22:08:50 +0200

    newUpdates

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,11 +1,48 @@
-const ImageGallery = () => {
-  return (
-    <ul className="ImageGallery">
-      <li> </li>
-    </ul>
-  );
-};
-export default ImageGallery;
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
+import Modal from '../Modal/Modal';
+import s from '../ImageGallery/ImageGallery.module.css';
+import { v4 as uuidv4 } from 'uuid';
+class ImageGallery extends Component {
+  state = {
+    showModal: false,
+    modalImage: '',
+    url: '',
+  };
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
+  };
+  openModal = modalImage => {
+    this.toggleModal();
 
-/* 
-<!-- Набор с изображениями --> */
+    this.setState({ modalImage });
+  };
+  render() {
+    const { images } = this.props;
+    const { showModal, modalImage } = this.state;
+    return (
+      <>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            {<img src={modalImage} alt="#" />}
+          </Modal>
+        )}
+        <ul className={s.gallery}>
+          {images.map(e => (
+            <ImageGalleryItem
+              key={uuidv4()}
+              url={e.webformatURL}
+              alt={e.tags}
+              modalImage={e.largeImageURL}
+              openModal={this.openModal}
+            />
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
+export default ImageGallery;
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,82 +1,43 @@
-//import { render } from "@testing-library/react";
-import { Component } from "react";
+import React, { Component } from 'react';
 
-// class SearchBar extends Component {
-//   //   loginInputId = shortid.generate();
-//   //   numberInputId = shortid.generate();
-//   //   objId = shortid.generate();
-//   state = {
-//     name: "",
-//     number: "",
-//   };
-//   handleChange = (event) => {
-//     console.log(event.currentTarget);
-//     console.log(event.currentTarget.name);
-//     console.log(event.currentTarget.value);
-
-//     this.setState({
-//       [event.currentTarget.name]: event.currentTarget.value,
-//     });
-//   };
-//   handleSubmit = (e) => {
-//     e.preventDefault();
-//     console.log(this.state);
-//     this.props.onSubmit(this.state);
-//     this.reset();
-//   };
-//   reset = () => {
-//     this.setState({ name: "", number: "" });
-//   };
-//   render() {
-//     return (
-//       <header className="Searchbar">
-//         <form className="SearchForm">
-//           <button type="submit" className="SearchForm-button">
-//             <span className="SearchForm-button-label">Search</span>
-//           </button>
-
-//           <input
-//             type="text"
-//             name="query"
-//             className="SearchForm-input"
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//           />
-//         </form>
-//       </header>
-//     );
-//   }
-// }
-
-// export default SearchBar;
-
-class SearchForm extends Component {
-  state = { query: "" };
-
-  handleChange = (e) => {
+class SearchBar extends Component {
+  state = { query: '' };
+  handleChange = e => {
     this.setState({ query: e.currentTarget.value });
   };
-
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
-
     this.props.onSubmit(this.state.query);
-    this.setState({ query: "" });
+    this.setState({ query: '' });
   };
 
+  // const { onDeleteTodo, completed, onToggleCompleted, } = onSubmit;
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          value={this.state.query}
-          onChange={this.handleChange}
-        />
-        <button type="submit">Искать</button>
-      </form>
+      <header className="Searchbar">
+        <form className="SearchForm" onSubmit={this.handleSubmit}>
+          <button
+            type="submit"
+            className="SearchForm-button"
+            // onClick={() => onDeleteTodo(id)}
+          >
+            <span className="SearchForm-button-label">Search</span>
+          </button>
+
+          <input
+            className="SearchForm-input"
+            value={this.state.query}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            // checked={completed}
+            onChange={this.handleChange}
+          />
+        </form>
+      </header>
     );
   }
 }
 
-export default SearchForm;
+export default SearchBar;
